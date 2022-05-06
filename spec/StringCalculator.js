3dfd2c054dbdc8b4128ecdeea482dd7cfed947ef
pravin-thinkster <69452514+pravin-thinkster@users.noreply.github.com>
2022-05-06 15:43:45 +0530

    Binary addition

--- spec/StringCalculator.js
+++ spec/StringCalculator.js
@@ -6,5 +6,7 @@ StringCalculator.prototype.add = function (numbers) {
     var operands = numbers.split(',');
     if (operands.length === 1) {
         return +numbers;
+    } else if (operands.length === 2) {
+        return (+operands[0]) + (+operands[1]);
     }
 };
--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -1,17 +1,23 @@
 describe('String Calculator', function () {
     var calc;
-  
-    beforeEach(function() {
-      calc = new StringCalculator();
+
+    beforeEach(function () {
+        calc = new StringCalculator();
     });
-  
+
     it('handles empty addition', function () {
-      expect(calc.add('')).toEqual(0);
+        expect(calc.add('')).toEqual(0);
     });
-  
+
     it('handles unary addition', function () {
-      expect(calc.add('133')).toEqual(133);
-      expect(calc.add('1')).toEqual(1);
-      expect(calc.add('-41')).toEqual(-41);
+        expect(calc.add('133')).toEqual(133);
+        expect(calc.add('1')).toEqual(1);
+        expect(calc.add('-41')).toEqual(-41);
     });
-  });
+
+    it('handles binary addition', function () {
+        expect(calc.add('133,36')).toEqual(169);
+        expect(calc.add('1,2')).toEqual(3);
+        expect(calc.add('-41,50')).toEqual(9);
+    });
+});
