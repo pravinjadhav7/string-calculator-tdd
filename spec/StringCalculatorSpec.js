dcffcc25fc81bb8fea07be5be1df2387e581771b
pravin-thinkster <69452514+pravin-thinkster@users.noreply.github.com>
2022-05-06 16:26:45 +0530

    Update code to be closer to spec and html

--- spec/StringCalculatorSpec.js
+++ spec/StringCalculatorSpec.js
@@ -26,18 +26,22 @@ describe('String Calculator', function () {
 
     it('handles delimiters of any length', function () {
         expect(calc.add('//[***]\n1***2***3')).toEqual(6);
-        expect(calc.add('//[,!,]\n1,!,2,!,90,!,100,!,1000,!,10000,!,100000')).toEqual(193);
+        expect(calc.add('//[,!,]\n1,!,2,!,90,!,100,!,1000,!,10000,!,100000')).toEqual(1193);
     });
   
     it('handles multiple custom delimiters', function () {
       expect(calc.add('//[*][%]\n1*2%3')).toEqual(6);
-      expect(calc.add('//[!!][^^]\n1!!2!!90!!100^^1000!!10000!!100000')).toEqual(193);    });
+      expect(calc.add('//[!!][^^]\n1!!2!!90!!100^^1000!!10000!!100000')).toEqual(1193);    });
 
     it('supports number strings delimited w/ newlines', function () {
         expect(calc.add('133\n36,1,3,5')).toEqual(178);
         expect(calc.add('//!\n' + '1!2!90!100!')).toEqual(193);
     });
 
+    it('ignores numbers bigger than 1000', function () {
+        expect(calc.add('133,36,1,3,5000')).toEqual(173);
+        expect(calc.add('1,2,90,100,1000,10000,100000')).toEqual(1193);
+    });
 
     it('dynamically changes the delimiter', function () {
         expect(calc.add('//;\n' + '133\n36,1,3;5')).toEqual(178);
@@ -52,9 +56,6 @@ describe('String Calculator', function () {
             }).toThrow('negatives not allowed -41 -20 -30');
         });
 
-        it('ignores numbers bigger than 1000', function () {
-            expect(calc.add('133,36,1,3,5000')).toEqual(173);
-            expect(calc.add('1,2,90,100,1000,10000,100000')).toEqual(193);
-        });
+
     });
 });
