2f4fd11ace043df7cf9c681d7ded1df01d3649e8
pravin-thinkster <69452514+pravin-thinkster@users.noreply.github.com>
2022-05-06 16:01:21 +0530

    Function comments

--- spec/StringCalculator.js
+++ spec/StringCalculator.js
@@ -1,19 +1,25 @@
+// Our Calculator object.
 var StringCalculator = function () {
 
 };
 
+/**
+ *  The add method, takes a string of numbers.
+ */
 StringCalculator.prototype.add = function (numbers) {
     var delimiters = '\n,';
 
+    // If there are custom delimiters, process them.
     if (numbers.indexOf('//') === 0) {
-      delimiters += numbers.substring(2, numbers.indexOf('\n'));
-      numbers = numbers.substring(numbers.indexOf('\n'));
+        delimiters += numbers.substring(2, numbers.indexOf('\n'));
+        numbers = numbers.substring(numbers.indexOf('\n'));
     }
-  
+
+    // Separate numbers using the delimiters.
     var operands = numbers.split(new RegExp('[' + delimiters + ']'));
 
-    
-     return operands.reduce(function (acc, cur) {
+    // Returns the sum of all the numbers.
+    return operands.reduce(function (acc, cur) {
         return acc + Number(cur);
     }, 0);
 };
