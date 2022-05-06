9d0a0ad6de09e943c9fc297286651208cf0ddb46
pravin-thinkster <69452514+pravin-thinkster@users.noreply.github.com>
2022-05-06 15:40:16 +0530

    Add first test case w/ empty string

---
+++ spec/StringCalculatorSpec.js
@@ -0,0 +1,11 @@
+describe("String Calculator", function() {
+    var calc;
+  
+    beforeEach(function() {
+      calc = new StringCalculator();
+    });
+  
+    it("handles empty addition", function() {
+      expect(calc.add('')).toEqual(0);
+    });
+  });
